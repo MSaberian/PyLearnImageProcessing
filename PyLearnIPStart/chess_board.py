ac6112b79fc3c10496c13095443d00157c930780
MSaberian <mo.saberian@yahoo.com>
2023-03-22 20:24:22 +0430

    change chess

--- PyLearnIPStart/chess_board.py
+++ PyLearnIPStart/chess_board.py
@@ -6,10 +6,7 @@ board = np.zeros((size*8, size*8))
 
 for i in range(8):
     for j in range(8):
-        for row in range(i*size,(i+1)*size):
-            for col in range(j*size,(j+1)*size):
-                board[row][col] = ((i+j)%2)*255
+        board[i*size:(i+1)*size,j*size:(j+1)*size] = ((i+j)%2)*255
 
 cv2.imshow('chess board', board)
 cv2.waitKey()
-
