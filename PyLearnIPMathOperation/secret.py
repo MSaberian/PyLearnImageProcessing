6052e51b54ff9427fe6dafd631ac26f3ff1ec5ee
MSaberian <mo.saberian@yahoo.com>
2023-04-12 14:27:32 +0430

    edit images

--- PyLearnIPMathOperation/secret.py
+++ PyLearnIPMathOperation/secret.py
@@ -6,4 +6,7 @@ secret_1 = cv2.imread('input/secret_1.jpg')
 
 secret = 255 - secret_1 + secret_0
 
-cv2.imwrite('output\sketch.jpg', secret)
+secrets = np.concatenate((secret_0, secret_1, secret), axis=1)
+
+cv2.imwrite('output\secret.jpg', secret)
+cv2.imwrite('output\secrets.jpg', secrets)
--- PyLearnIPMathOperation/virtual_decoration.py
+++ PyLearnIPMathOperation/virtual_decoration.py
@@ -4,14 +4,26 @@ import numpy as np
 image_floor = cv2.imread('input/floor.jpg')
 image_new_floor = cv2.imread('input/new_floor.jpg')
 image_room = cv2.imread('input/room_background.jpg')
+image_room0 = image_room
 
 image_floor = image_floor / 255
 
-image_room = image_room*(1-image_floor)
+image_room0 = image_room0*(1-image_floor)
 
 result = np.zeros(image_floor.shape)
 
-result = image_floor*image_new_floor + image_room
-
+result = image_floor*image_new_floor + image_room0
 
 cv2.imwrite('output/New_room.jpg',result)
+result = cv2.resize(result, [result.shape[1]//3, result.shape[0]//3])
+image_floor = cv2.resize(image_floor, [image_floor.shape[1]//3, image_floor.shape[0]//3])
+cv2.imwrite('output/floor_small.jpg',image_floor*255)
+image_new_floor = cv2.resize(image_new_floor, [image_new_floor.shape[1]//3, image_new_floor.shape[0]//3])
+cv2.imwrite('output/new_floor_small.jpg',image_new_floor)
+image_room = cv2.resize(image_room, [image_room.shape[1]//3, image_room.shape[0]//3])
+cv2.imwrite('output/room_small.jpg',image_room)
+
+image_row_0 = np.concatenate((image_floor*255, image_new_floor), axis=1)
+image_row_1 = np.concatenate((image_room, result), axis=1)
+room_concatenate = np.concatenate((image_row_0, image_row_1), axis=0)
+cv2.imwrite('output/room_concatenate.jpg',room_concatenate)
