aed787912309f458b9efe6cb6d09a89eb3357a04
MSaberian <mo.saberian@yahoo.com>
2023-05-05 11:55:25 +0430

    edge detection

--- PyLearnIPConvolution/blurring.py
+++ PyLearnIPConvolution/blurring.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 image = cv2.imread('input/flower.jpg', cv2.IMREAD_GRAYSCALE)
-image = image[500:700,500:700]
+# image = image[500:700,500:700]
 rows, cols = image.shape
 FS = 39 # filter size
 print(image.shape)
@@ -19,18 +19,18 @@ for i in range(FS//2, rows-FS//2):
         small = image[(i-FS//2):(i+FS//2+1),(j-FS//2):(j+FS//2+1)]
         average = np.sum(filter * small)
         if average <128 or image[i,j] < 128:
-            if average > 42: 
-                small[small>32] = 16
-                average2 = np.sum(filter * small)
-                image_new[i, j] = average2
-            else:
-                image_new[i, j] = average
+            # if average > 42: 
+            #     small[small>32] = 16
+            #     average2 = np.sum(filter * small)
+            #     image_new[i, j] = average2
+            # else:
+            image_new[i, j] = average
         else:
             # image = cv2.imread('input/flower.jpg', cv2.IMREAD_GRAYSCALE)
             # image = image[500:700,500:700]
             image_new[i, j] = image[i,j]
         
-cv2.imwrite('output/flower_blur4.png',image_new)
-cv2.imwrite('output/imageorg.png',image)
+cv2.imwrite('output/flower_blur.png',image_new)
+# cv2.imwrite('output/imageorg.png',image)
 
 # cv2.waitKey()
