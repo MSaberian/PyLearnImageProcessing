4c59da29d27e2460d41fe156b0e9a7a47700dea8
MSaberian <mo.saberian@yahoo.com>
2023-05-06 20:03:00 +0430

    hist

--- PyLearnIPHistogramEqualization/histogram.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-
-image = cv2.imread('input\lady_2.jpg', cv2.IMREAD_GRAYSCALE)
-
-
-hist = cv2.calcHist([image], [0], None, [256], [0, 256])
-plt.plot(hist)
-plt.show()
