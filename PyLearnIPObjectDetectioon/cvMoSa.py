0f2722b30bcc0ec2cca68c55a35abe84dcb85016
MSaberian <mo.saberian@yahoo.com>
2023-05-17 17:56:11 +0430

    finish area

--- PyLearnIPObjectDetectioon/cvMoSa.py
+++ PyLearnIPObjectDetectioon/cvMoSa.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def boundingRectMoSa(contour):
+    
     x = np.min(contour[:,0,0])
     w = np.max(contour[:,0,0]) - x + 1
     y = np.min(contour[:,0,1])
@@ -18,12 +19,32 @@ def contourAreaMoSa(contour):
     points = []
     for i in range(x, x+w):
         cross = 0
+        temp_area = 0
+        temp_points = []
+        last_state = False
         for j in range(y, y+h):
-            point = [i, j]  
-            print(point in contour_)
-            
-            # if big and small:
-            #     area += 1
-            #     points.append([i, j])
+            if ([i, j] in contour_):
+                if not last_state:
+                    cross +=1
+                last_state = True
+            elif last_state:
+                last_state = False
+                if i:
+                    if [i-1, j] in points:
+                        cross = 1
+                    else:
+                        cross = 0
+            else:
+                last_state = False
+            if cross % 2 == 1:
+                temp_area +=1
+                temp_points.append([i,j])
+            else:
+                if(temp_area):
+                    for k in range(temp_area):
+                        points.append(temp_points[k])
+                area += temp_area
+                temp_area = 0
+                temp_points = []
 
     return area, points
